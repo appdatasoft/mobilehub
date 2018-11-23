dad991c173489851641c9c78748c320789d6f8a3
appdatasoft <sumi@appdatasoft.com>
2018-11-22 21:08:45 -0800

    aws-exports.js

---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-container>
+    <div class="b-main-content">
+      <h2>HUB for Software Engineers</h2>
+      <p>You are logged in as {{ user.username }}.</p>
+    </div>
+  </b-container> 
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+    computed: {
+        ...mapState({
+            user: state => state.auth.user,
+        })
+    }
+}
+</script>
+
