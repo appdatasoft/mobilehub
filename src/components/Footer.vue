cf14c5b88bcfc69d2f0557811c5941174883f396
appdatasoft <sumi@appdatasoft.com>
2018-11-30 20:47:19 -0800

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,27 +1,21 @@
 <template>
-  <footer class="bd-footer">
-    <b-container>
-      <b-row class="justify-content-md-center">
-        <div class="bd-footer-copyright">© 2018 Hack-o-hire.</div>
-      </b-row>
-    </b-container>
-  </footer>
-</template>
-
-<script>
-export default {
-    
-}
-</script>
-
-<style>
-.bd-footer {
-  margin-top: 2em;
-  border-top: .2rem #f7f7f7 solid !important;
-}
-
-.bd-footer-copyright {
-  padding-top: 1rem;
-  padding-bottom: 1rem;
-}
-</style>
+<b-container>
+<footer>
+      <div class="container">
+        <p>&copy; hackohire 2018. All Rights Reserved.</p>
+        <ul class="list-inline">
+          <li class="list-inline-item">
+            <a href="https://appdatasoft0-my.sharepoint.com/:b:/g/personal/sumi_appdatasoft_com/EVLkdrSuTZ9AnYCEP2RTlD8BOEp-T3AYsKFgeP2sFDBJng?e=ooK7VV">Privacy</a>
+          </li>
+          <li class="list-inline-item">
+            <a href="https://appdatasoft0-my.sharepoint.com/:w:/g/personal/sumi_appdatasoft_com/EYbol7k-QVBPutYN3CsHTEIBMKiZqeHMpx08W4YnWNuCkg?e=DtoU77">Terms</a>
+          </li>
+          <li class="list-inline-item">
+            <a href="#">FAQ</a>
+          </li>
+        </ul>
+      </div>
+       </footer>
+      </b-container>
+   
+    </template>
