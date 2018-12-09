92c8ddec290649d4eae08bc30ce967c9df32c3ef
appdatasoft <sumi@appdatasoft.com>
2018-12-08 22:34:41 -0800

    Hiring.vue update

--- src/pages/Hiring.vue
+++ src/pages/Hiring.vue
@@ -1,91 +1,59 @@
 <template>
 <div class="ui six column grid" >
-  <div class="column" id="side_bar">
-    <div class="ui compact ">
-      <div class="ui vertical menu">
-  <div class="item">
-    <div class="ui input"><input type="text" placeholder="Search..."></div>
-  </div>
-  <div class="item">
-    Home
-    <div class="menu">
-      <a class="active item">Search</a>
-      <a class="item">Add</a>
-      <a class="item">Remove</a>
-    </div>
-  </div>
-  <a class="item">
-    <i class="grid layout icon"></i> Browse
-  </a>
-  <a class="item">
-    Messages
-  </a>
-  <a class="item">
-    Details
-  </a>
-  <a class="item">
-    jobs
-  </a>
-  <div class="item">
-    pages
-    <div class="menu">
-      <a class="active item">Search</a>
-      <a class="item">Add</a>
-      <a class="item">Remove</a>
-    </div>
-  </div>
-  <a class="item">
-    Settings
-  </a>
-  <div class="ui dropdown item">
-    More
-    <i class="dropdown icon"></i>
-    <div class="menu">
-      <a class="item"><i class="edit icon"></i> Edit Profile</a>
-      <a class="item"><i class="globe icon"></i> Choose Language</a>
-      <a class="item"><i class="settings icon"></i> Account Settings</a>
-        </div>
-      </div>
-   </div>
-    <!-- verical-column -->
-    <div class="ui vertical menu">
-  <div class="item">
-    <div class="header">Products</div>
-    <div class="menu">
-      <a class="item">Enterprise</a>
-      <a class="item">Consumer</a>
-    </div>
-  </div>
-  <div class="item">
-    <div class="header">CMS Solutions</div>
-    <div class="menu">
-      <a class="item">Rails</a>
-    </div>
-  </div>
-    </div>
-
-
-
- </div>
-</div>
- 
+ <div class="column" id="side_bar">   
+   <v-menuleft/>
+</div>  
  <!-- column -2 -->
  
 <div class="column" id="column_2">
 <!-- <div class="ui compact segment" id="column_2_segment"> -->
- 
   <div class="ui stackable grid container ">
     <div class="fourteen wide column">
+
+<div class="ui four steps">
+  <a href="/Job_Posts" class="active step">
+    <i class="paste icon "></i>
+    
+    <div class="content">
+      <div class="title">Job Posts</div>
+      <div class="description">Job related posts</div>
+    </div>
+  </a>
+  <a href="/Candidates" class="active  step">
+    <i class="user  icon"></i>
+    <div class="content">
+      <div class="title">Candidates</div>
+      <div class="description">Enter billing information</div>
+    </div>
+  </a>
+  <a href="/Int_Hackathon" class=" active  step">
+    <i class="hourglass start icon"></i>
+               <div class="content">
+           <div class="title">Interview Hackathon</div>
+         <div class="description">Verify order details</div>
+       </div>
+     </a>
+     <a class="active step">
+    <i class="handshake icon"></i>
+               <div class="content">
+           <div class="title">Hiring</div>
+         <div class="description">Verify order details</div>
+       </div>
+     </a>
+  </div>
+       <div class="ui segment">
        <div class="ui pointing menu">
   <a class="item active">Hiring</a>
   <a class="item">Job-post</a>
   <a class="item"> Candidates</a>
   <a class="item">Interview Hackathon</a>
 </div>
-
 <div class="ui form">
-            <textarea  class="field" rows="6" placeholder="A breif bio of you"></textarea>
+            
+<textarea  class="field" rows="6" placeholder="A breif bio of you"></textarea>
+           
             </div>
+       </div>
       <div class="ui segment">
         <h2 class="ui dividing header">Create Job Openning</h2>
         <form class="ui form">
